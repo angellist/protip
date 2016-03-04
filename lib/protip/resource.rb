89fbf7420c97681833c2fa0bbb6a122c0fe9695d
Kevin Montag <kmontag@cs.stanford.edu>
2016-03-03 18:49:25 -0800

    Add changes_applied function back for older ActiveModel versions

--- lib/protip/resource.rb
+++ lib/protip/resource.rb
@@ -326,5 +326,12 @@ module Protip
     def errors
       @errors ||= ActiveModel::Errors.new(self)
     end
+
+    private
+    # needed for ActiveModel::Dirty in earlier ActiveModel versions
+    def changes_applied
+      @previously_changed = changes
+      @changed_attributes.clear
+    end
   end
 end
