c00a9527079293ee6f9e456196ed64ba2251888f
Kevin Montag <kmontag@cs.stanford.edu>
2016-03-09 16:02:42 -0800

    Remove unnecessary enum method

--- lib/protip.rb
+++ lib/protip.rb
@@ -5,20 +5,3 @@ require 'protip/resource'
 if defined?(Mime::Type)
   Mime::Type.register 'application/x-protobuf', :protobuf
 end
-
-module Protip
-  # Temporary placeholder until Ruby support for field options in protobuf is
-  # rolled out. Once that happens, this will be configurable in protobuf:
-  #
-  # = Protobuf Example
-  #
-  #   message Foo {
-  #     enum State {
-  #       CREATED=0;
-  #     }
-  #     protip.messages.EnumValue state = 1 [(protip_enum) = "Foo.State"]
-  #   }
-  def self.set_enum(field, enum_name)
-    field.instance_variable_set :'@_protip_enum_name', enum_name.to_s
-  end
-end
