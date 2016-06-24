2dea1f3decb2e5fe3a2ef0040f1ce91643dbb25a
Kevin Montag <kmontag@cs.stanford.edu>
2016-06-23 20:08:05 -0700

    Some extra BigDecimal test cases

--- test/unit/protip/transformers/big_decimal_transformer_test.rb
+++ test/unit/protip/transformers/big_decimal_transformer_test.rb
@@ -16,15 +16,32 @@ describe Protip::Transformers::BigDecimalTransformer do
   # Since we're just serializing/deserializing values, we test both
   # transformer methods in tandem.
   describe 'transformation' do
-    def self.it_transforms(name, value)
+    # Assumes expected_value = value unless it is explicitly provided
+    def self.it_transforms(name, value, expected_value = nil)
+      expected_value ||= value
       it "transforms #{name} values" do
         message = transformer.to_message(value, field)
-        assert_equal value, transformer.to_object(message, field)
+        assert_equal expected_value, transformer.to_object(message, field)
       end
     end
-    it_transforms 'integer', BigDecimal.new(104, 1)
-    it_transforms 'infinity', (BigDecimal.new(1, 1) / 0)
-    it_transforms 'fractions', BigDecimal.new(100.5, 5)
-    it_transforms 'rational numbers', BigDecimal.new(Rational(2, 3), 3)
+    describe 'for BigDecimal arguments' do
+      it_transforms 'integer', BigDecimal.new(104, 1)
+      it_transforms 'infinity', (BigDecimal.new(1, 1) / 0)
+      it_transforms 'fractions', BigDecimal.new(100.5, 5)
+      it_transforms 'rational numbers', BigDecimal.new(Rational(2, 3), 3)
+    end
+
+    describe 'for non-BigDecimal arguments' do
+      it_transforms 'integer', 3, BigDecimal.new(3, 1)
+      it_transforms 'string', '3.3', BigDecimal.new(3.3, 2)
+
+      # Match standard BigDecimal behavior for floats
+      it 'raises an argument error for floats' do
+        exception = assert_raises ArgumentError do
+          transformer.to_message(5.5, field)
+        end
+        assert_equal "can't omit precision for a Float.", exception.message
+      end
+    end
   end
 end
